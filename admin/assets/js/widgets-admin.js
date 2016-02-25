136d4b03051ad734d094a9de086b151b573cbc2f
Mattia Migliorini <mattia@squeezyweb.com>
2016-02-25 17:29:43 +0100

    Improve widgets-admin.js Fix #14

--- admin/assets/js/widgets-admin.js
+++ admin/assets/js/widgets-admin.js
@@ -2,28 +2,37 @@
 	var types = new Array( 'pin', 'profile', 'board' );
 
 	function onLoad() {
-		var checked = $('input[name="widget-pit_pinterest[2][select]"]:radio:checked').val();
-		$.each( types, function() {
-			if ( this == checked )
-				return;
-			$('.' + this + '-control').hide();
-			$('.' + this + '-help').hide();
+		$('.widget[id*="pit_pinterest"]').each(function() {
+			var widget = $(this);
+			var type = widget.find('input[type=radio]:checked').val();
+			showHideFields(widget, type);
 		});
-		$('.' + checked + '-control').show();
-		$('.' + checked + '-help').show();
+	}
+
+	function showHideFields(widget, type) {
+		$.each( types, function() {
+			if ( this == type )
+				return;
+			widget.find('.' + this + '-control').hide();
+			widget.find('.' + this + '-help').hide();
+		});
+		widget.find('.' + type + '-control').show();
+		widget.find('.' + type + '-help').show();
+	}
+
+	function addListener() {
+		$('.widget[id*="pit_pinterest"]').on( 'change', 'input[type=radio]', function() {
+			var widget = $(this).parents('.widget');
+			var type = $(this).val();
+			showHideFields(widget, type);
+		});
 	}
 
 	$(document).ready( onLoad );
 	$(document).ajaxComplete( onLoad );
 
-	$('#widgets-right').on( 'change', 'input[name="widget-pit_pinterest[2][select]"]:radio', function() {
-		var checked = $(this).val();
-		$.each( types, function() {
-			if ( this == checked ) return;
-			$('.' + this + '-control').hide();
-			$('.' + this + '-help').hide();
-		});
-		$('.' + checked + '-control').show();
-		$('.' + checked + '-help').show();
-	});
+	$(document).ready( addListener );
+	// The following attaches the event listener to newly added widgets, but also
+	// to already existing widgets. This may cause performance issues.
+	$(document).ajaxComplete( addListener );
 })(jQuery);
